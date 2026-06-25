58dc7fd64b62e2e9188e1f2b020bfc50672f1773
Nitesh <mishranitesh45176@gmail.com>
2026-06-25 23:19:28 +0530

    Added Backend deployed url

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,16 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.upload import router as upload_router
 
-
 app = FastAPI(
     title="Bank Statement Analyzer"
 )
 
+origins = [
+    "http://localhost:5173",
+    "https://bank-statement-analyzer-sandy.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
